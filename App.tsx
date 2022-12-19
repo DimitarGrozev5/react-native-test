07d83f5515a9c802e018a849971dd1fb28dfd152
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-12-19 11:27:49 +0200

    removed redux

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
-import { Provider } from 'react-redux';
 import AppLayout from './src/components/app-layout';
 import DataOverview from './src/components/data-overview/DataOverview';
 import GoalControl from './src/components/goal-control/GoalControl';
 import SessionControl from './src/components/session-control/SessionControl';
 import StoreProvider from './src/store-mobx/storeContext';
-import store from './src/store/store';
 
 export default function App() {
   return (
     <StoreProvider>
-      <Provider store={store}>
-        <AppLayout>
-          <SessionControl />
-          <GoalControl />
-          <DataOverview />
-        </AppLayout>
-      </Provider>
+      <AppLayout>
+        <SessionControl />
+        <GoalControl />
+        <DataOverview />
+      </AppLayout>
     </StoreProvider>
   );
 }
