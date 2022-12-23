de53c8d1e9c4673815e028bf60f497e11b461820
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-12-23 14:41:41 +0200

    add comments

--- src/components/session-control/SessionControl.tsx
+++ src/components/session-control/SessionControl.tsx
@@ -14,19 +14,24 @@ import CenteredText from '../views/CenteredText';
 import AccentText from '../views/AccentText';
 
 const SessionControl = observer(() => {
+  // Get values from store
   const activeSession = useDBStore('activeSession');
   const goals = useDBStore('goals');
   const today = useDBStore('achieved').today;
 
+  // State for the component
   const [sessionState, setSessionState] = useState<SessionState>({
     active: false,
   });
 
+  // Helper values, for controlling auto start and stop
   const [requestedAutoStart, setRequestedAutoStart] = useState(false);
   const [startedAutoStart, setStartedAutoStart] = useState(false);
 
+  // Get the device orientation
   const downOrUp = useOrientation();
 
+  // Setup timer
   useEffect(() => {
     let timer: number;
     if (activeSession.startedAt !== null) {
@@ -42,6 +47,8 @@ const SessionControl = observer(() => {
     };
   }, [activeSession, activeSession.startedAt, goals, today, sessionState]);
 
+  /// Handler functions
+  // Stop the active session
   const stopSessionHandler = useCallback(() => {
     const now = new Date().getTime() / 1000;
     const startTime = activeSession.startedAt || now;
@@ -51,6 +58,7 @@ const SessionControl = observer(() => {
     today.addToAchieved(totalSessionTime);
   }, [activeSession, today]);
 
+  // Update the session duration with a user provided value
   const wrongTimeHandler = useCallback(
     (time: Seconds) => {
       activeSession.setStartedAt(null);
@@ -59,16 +67,18 @@ const SessionControl = observer(() => {
     [activeSession, today]
   );
 
+  // Start the session
   const startSessionHandler = useCallback(() => {
     const now = new Date().getTime() / 1000;
     activeSession.setStartedAt(now);
   }, [activeSession]);
 
+  // Start listening for auto start
   const startWithGesturesHandler = () => {
     setRequestedAutoStart(true);
   };
 
-  // Handle device orientation for auto start and stop
+  // Auto start and stop, based on device orientation
   useEffect(() => {
     if (requestedAutoStart) {
       if (downOrUp === 'down' && !startedAutoStart) {
@@ -114,12 +124,12 @@ const SessionControl = observer(() => {
         startSessionHandler={startSessionHandler}
         startWithGesturesHandler={startWithGesturesHandler}
       />
-      
+
       {requestedAutoStart && !startedAutoStart && (
-          <CenteredText>
-            <AccentText>Put the phone down to start the timer</AccentText>
-          </CenteredText>
-        )}
+        <CenteredText>
+          <AccentText>Put the phone down to start the timer</AccentText>
+        </CenteredText>
+      )}
     </TodayOverview>
   );
 });
