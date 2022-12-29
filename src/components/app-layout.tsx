3943a1b2bfc1cccd98279de3eba77bf50295861d
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-12-29 13:53:25 +0200

    add keyboardAvoidingView to app layout

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, StatusBar as SB } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
 import { Colors } from '../global-styling';
 
 type Props = React.PropsWithChildren;
 
 const AppLayout: React.FC<Props> = ({ children }) => {
   return (
-    <SafeAreaView style={[styles.container, { paddingTop: SB.currentHeight }]}>
-      <StatusBar style="dark" />
-      {children}
-    </SafeAreaView>
+    <KeyboardAvoidingView behavior="height" style={styles.screen}>
+      <View style={[styles.container]}>
+        <StatusBar style="dark" />
+        {children}
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default AppLayout;
 
 const styles = StyleSheet.create({
-  safeArea: {
+  screen: {
     flex: 1,
+    borderWidth: 3,
+    borderColor: 'red',
   },
   container: {
-    flex: 1,
     backgroundColor: Colors.primary300,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingHorizontal: 16,
-    paddingBottom: 24,
+    padding: 16,
   },
 });
